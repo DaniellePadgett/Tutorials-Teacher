57d44b6215beda46564512ca5aaabceb552d7294
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-22 09:57:50 +0100

    Annotations

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using System; //.NET Framework Namespace
 
-namespace Tutorials_Teacher
+namespace Tutorials_Teacher //Namespace Name
 {
-	class Program
+	class Program //Class Name
 	{
-		static void Main(string[] args)
+		static void Main(string[] args) //Method
 		{
 
-			string message = "Hello";
+			string /*Data Type*/ message /*Variable Name*/ = "Hello" /*Variable Value*/;
 
-			Console.WriteLine(message);
+			Console.WriteLine(message) /*Method to display value on the console*/;
 		}
 	}
 }
