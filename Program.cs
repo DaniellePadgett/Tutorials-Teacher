c8d695fd49a1181629dabb74f4fec4dc51b8aeb0
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-22 12:38:56 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System; //.NET Framework Namespace
+﻿using System;
 
 namespace Tutorials_Teacher //Namespace Name
 {
@@ -10,6 +10,18 @@ namespace Tutorials_Teacher //Namespace Name
 			string /*Data Type*/ message /*Variable Name*/ = "Hello" /*Variable Value*/;
 
 			Console.WriteLine(message) /*Method to display value on the console*/;
+
+			@class.NumberProperty = 100; /*Setting the number property from the other class to 100*/
+
+			Console.WriteLine(@class.NumberProperty);
 		}
 	}
+
+	public class @class /* calling a class "class" when prefixed with an @*/
+	{
+		public static int NumberProperty { get; set; }
+
+	}
+
+
 }
