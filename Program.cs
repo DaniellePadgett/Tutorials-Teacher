65ac6342a4cdfebe99831b79b56fc4d33c8ee929
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-22 09:53:07 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Tutorials_Teacher
+﻿using System;
+
+namespace Tutorials_Teacher
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+
+			string message = "Hello";
+
+			Console.WriteLine(message);
 		}
 	}
 }
